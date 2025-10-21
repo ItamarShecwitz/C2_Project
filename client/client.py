d846cc315d4ff11b712948ab3c937730ce158bea
Itamars <TheWaterPaper11@gmail.com>
2025-10-22 01:20:47 +0300

    divide to functions

--- client/client.py
+++ client/client.py
@@ -9,38 +9,60 @@ MAX_BYTES_REPONSE = 65536
 ENCODING = 'utf-8'
 STOP_WORD = "stop"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+def create_tcp_socket():
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def create_session_client(socket_object, client_host, server_host, port):
     # Connect to the server
-    s.connect((SERVER_HOST, PORT))
+    socket_object.connect((server_host, port))
 
     # Creating and sending a registration request to the server (The request is the client's host).
-    registration_request = CLIENT_HOST
-    s.sendall(bytes(registration_request, encoding=ENCODING))
+    registration_request = client_host
+    socket_object.sendall(bytes(registration_request, encoding=ENCODING))
 
-    # Running until "stop" is sent from the server.
-    while True:
+def execute_message(socket_object, message):
+    # If the message exist, execute it.
+    if message:
+        try:
+            result = subprocess.run(message, capture_output=True, text=True, shell=True, timeout = 5)
+            return result
+        except Exception as e:
+            # Handle errors
+            socket_object.sendall(bytes(f"Error executing command: {e}", encoding=ENCODING))
+            return None
+    else:
+        socket_object.sendall(bytes("Error with recieving message", encoding=ENCODING))
+        return None
+    
+def send_response(socket_object, result):
+    if result:
+        # Send the stdout, and stderr to the server.
+        response = (result.stdout + result.stderr).strip()
+        socket_object.sendall(bytes(response, encoding=ENCODING))
 
-        # Wait, and recieve the message from the server.
-        message = s.recv(MAX_BYTES_REPONSE).decode(ENCODING)
 
-        # If the message is stop, stop the session.
-        if message == STOP_WORD:
-            break
 
-        # If the message exist, execute it.
-        if message:
-            print(message)
-            try:
-                result = subprocess.run(message, capture_output=True, text=True, shell=True, timeout=5)
-            except Exception as e:
-                # Handle errors
-                s.sendall(bytes(f"Error executing command: {e}", encoding=ENCODING))
-                continue
+def main():
+    with create_tcp_socket() as s:
 
-            # Send the stdout, and stderr to the server.
-            response = (result.stdout + result.stderr).strip()
-            s.sendall(bytes(response, encoding=ENCODING))
-        else:
-            s.sendall(bytes("Error with recieving message", encoding=ENCODING))
+        create_session_client(s, CLIENT_HOST, SERVER_HOST, PORT)
+        
+        # Running until "stop" is sent from the server.
+        while True:
+
+            # Wait, and recieve the message from the server.
+            message = s.recv(MAX_BYTES_REPONSE).decode(ENCODING)
+
+            # If the message is stop, stop the session.
+            if message == STOP_WORD:
+                break
+            
+            result = execute_message(s, message)
+
+            send_response(s, result)
+
 
 print("Done")
+
+if __name__ == "__main__":
+    main()
--- server/server.py
+++ server/server.py
@@ -7,33 +7,63 @@ MAX_BYTES_REPONSE = 65536
 ENCODING = 'utf-8'
 STOP_WORD = "stop"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
+def create_tcp_socket():
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def bind_and_listen(socket_object, server_host, port):
     # Binding the server's host.
-    s.bind((SERVER_HOST, PORT))
+    socket_object.bind((server_host, port))
 
     # Establish new connection.
-    s.listen()
-    connection, address = s.accept()
+    socket_object.listen()
+
+
+def create_session_server(socket_object):
+    connection, _ = socket_object.accept()
     client_host = connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
+    return connection
 
-    with connection:
+def send_new_message(connection):
+    # Sending the message to the client.
+    message = input()
 
-        # Running until "stop" is sent.
-        while True:
+    if message:
+        connection.send(bytes(message, encoding=ENCODING))
 
-            # Sending the message to the client.
-            message = input()
+    return message
 
-            if message:
-                connection.send(bytes(message, encoding=ENCODING))
+
+def get_output(connection):
+    # Get the response from the client (stdout and stderr), and print it.
+    response = connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
+    print(response)
+
+
+def main():
+    with create_tcp_socket() as s:
+
+        bind_and_listen(s, SERVER_HOST, PORT)
+
+        connection = create_session_server(s)
+        
+        with connection:
+
+            # Running until "stop" is sent.
+            while True:
+
+                message = send_new_message(connection)
 
                 # If the message is stop, stop the session.
                 if message == STOP_WORD:
                     break
+                elif message:
+                    get_output(connection)
 
-                # Get the response from the client (stdout and stderr), and print it.
-                response = connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
-                print(response)
+    print("Done")
 
-print("Done")
+
+
+if __name__ == "__main__":
+    main()

ca1f4831a7aa48acff86bd99a4b28206c36f8fbc
Itamars <TheWaterPaper11@gmail.com>
2025-10-22 09:22:57 +0300

    simple logging

--- server/server.py
+++ server/server.py
@@ -5,14 +5,15 @@ SERVER_HOST = "127.0.0.1"
 PORT = 2222
 
 MAX_BYTES_REPONSE = 65536
-ENCODING = 'utf-8'
+ENCODING = "utf-8"
 STOP_WORD = "stop"
+PROMPT = "> "
 
 class Session():
     
     id_counter = 1
 
-    def __init__(self, socket_object):
+    def __init__(self, logger, socket_object):
         connection, _ = socket_object.accept()
 
         self.id = Session.id_counter
@@ -21,65 +22,83 @@ class Session():
         self.connection = connection
         self.client_host = connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
 
+        print_log(logger, f"Client connected: {self.client_host} (Session ID: {self.id})", self)
 
-    def send_new_message(self):
+
+    def send_new_message(self, logger):
         # Sending the message to the client.
-        message = input()
+        message = input(PROMPT)
 
         if message:
             self.connection.send(bytes(message, encoding=ENCODING))
+            print_log(logger, f"Sent: {message}", self, False)
 
         return message
 
 
-    def get_output(self):
+    def get_output(self, logger):
         # Get the response from the client (stdout and stderr), and print it.
-        return self.connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
+        response = self.connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
+        print_log(logger, f"Recived: {response}", self)
+        return response
+    
+    def stop_connection(self, logger):
+        self.connection.close()
+        print_log(logger, f"Client disconnected: {self.client_host} (Session ID: {self.id})", self)
 
 
-def create_tcp_socket():
-    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def create_tcp_socket(logger):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print_log(logger, "Server Started")
+    return server_socket
 
 
-def bind_and_listen(socket_object, server_host, port):
+def bind_and_listen(logger, socket_object, server_host, port):
     # Binding the server's host.
     socket_object.bind((server_host, port))
 
-    # Establish new connection.
+    # Listen to client.
+    print_log(logger, "Waiting for connections...")
     socket_object.listen()
 
   
 
-def print_logging(logger, log, session):
-    logger.info(log, extra={'address': session.client_host, 'session_id': session.id})
+def print_log(logger, log, session=None, printable=True):
+    if printable: print(log) 
+    if session:
+        logger.info(log, extra={"address": session.client_host, "session_id": session.id})
+    else:
+        logger.info(log, extra={"address": SERVER_HOST, "session_id": "server"})
 
 
 def main():
 
     #  Create logger
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='server.log', format='%(asctime)s - %(address)s (%(session_id)s) - %(message)s', level=logging.INFO)
+    logging.basicConfig(filename="server.log", format="%(asctime)s - %(address)s [%(session_id)s] - %(message)s", level=logging.INFO)
 
+    
 
+    with create_tcp_socket(logger) as server_socket:
 
-    with create_tcp_socket() as server_socket:
+        bind_and_listen(logger, server_socket, SERVER_HOST, PORT)
 
-        bind_and_listen(server_socket, SERVER_HOST, PORT)
-
-        session1 = Session(server_socket)
+        session1 = Session(logger, server_socket)
 
         with session1.connection:
 
             # Running until "stop" is sent.
             while True:
-                message = session1.send_new_message()
+                message = session1.send_new_message(logger)
 
                 # If the message is stop, stop the session.
                 if message == STOP_WORD:
                     break
                 elif message:
-                    print(session1.get_output())
+                    session1.get_output(logger)
                     
+            session1.stop_connection(logger)
 
     print("Done")
 

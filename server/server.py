6681fe6564ee802d116703e90570a60c74c04bcf
Itamars <TheWaterPaper11@gmail.com>
2025-10-23 18:57:09 +0300

    Two sessions in the same time

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 import socket
 import logging
-
+import threading
 
 # Global Constants
 SERVER_HOST = "127.0.0.1"
@@ -13,6 +13,9 @@ LOG_FILE_NAME = "server.log"
 LOG_FORMAT = "%(asctime)s - %(address)s [%(session_id)s] - %(message)s"
 LOG_DEFAULT_LEVEL = logging.INFO
 
+sessions = []
+main_session = None
+main_session_ready = threading.Event()
 
 class Session():
     # Represents a single active client session with the server.
@@ -20,10 +23,8 @@ class Session():
     id_counter = 1
 
 
-    def __init__(self, logger, socket_object):
+    def __init__(self, logger, connection):
         # Initialize a session for a connected client.
-        
-        connection, _ = socket_object.accept()
 
         self.id = Session.id_counter
         Session.id_counter += 1
@@ -53,7 +54,8 @@ class Session():
         response = self.connection.recv(MAX_BYTES_REPONSE).decode(ENCODING)
         print_log(logger, f"Recived: {response}", self)
         return response
-    
+
+
     def stop_connection(self, logger):
         # Close the connection of a session.
 
@@ -70,12 +72,27 @@ def create_tcp_socket(logger):
 
 
 def bind_and_listen(logger, socket_object, server_host, port):
-    # Binding the server's host, and isten to client.
+    # Binding the server's host, and listen to client.
 
     socket_object.bind((server_host, port))
 
     print_log(logger, "Waiting for connections...")
     socket_object.listen()
+
+
+def wait_for_new_connections(logger, socket_object):
+
+    global main_session
+    global main_session_ready
+
+    while True:
+        connection, _ = socket_object.accept()
+        new_session = Session(logger, connection)
+        sessions.append(new_session)
+        if len(sessions) == 1:
+            main_session = new_session
+            main_session_ready.set()
+
 
 def create_logger():
     # Create a new logger
@@ -104,20 +121,21 @@ def main():
     with create_tcp_socket(logger) as server_socket:
 
         bind_and_listen(logger, server_socket, SERVER_HOST, PORT)
-
-        session1 = Session(logger, server_socket)
+        wait_for_connections_thread = threading.Thread(target=wait_for_new_connections, args=(logger, server_socket))
+        wait_for_connections_thread.start()
+        main_session_ready.wait()
 
         # Running until "stop" is sent.
         while True:
-            message = session1.send_new_message(logger)
+            message = main_session.send_new_message(logger)
 
             # If the message is stop, stop the session.
             if message == STOP_WORD:
                 break
             elif message:
-                session1.get_output(logger)
+                main_session.get_output(logger)
                     
-        session1.stop_connection(logger)
+        main_session.stop_connection(logger)
 
 
 if __name__ == "__main__":
